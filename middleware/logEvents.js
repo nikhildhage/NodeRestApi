7048ab001306aef2e3d524d06d751cd56dbcfa69
nikhildhage <nikhil.dhage@gmail.com>
2024-05-08 08:47:08 -0700

    Added log Events js file

---
+++ middleware/logEvents.js
@@ -0,0 +1,43 @@
+//NPM mOdule imports
+const { format } = require("date-fns");
+const { v4: uuidv4 } = require("uuid");
+
+//Core NOde Modules
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+// Log Events Function
+
+/**
+ * Check if Logs folder exists
+ * If not create it
+ * Then check for eventLogs.txt file.
+ * If eventLog.txt file exists
+ * then log events
+ **/
+
+const logEvents = async (message) => {
+	const dateTime = `${format(new Date(), "MM-dd-yyyy \t hh:mm:ss")}`;
+	const logItem = `${dateTime} \t ${uuidv4()} \t ${message} \n`;
+	console.log(logItem);
+
+	try {
+		if (!fs.existsSync(path.join(__dirname, "logs"))) {
+			await fsPromises.mkdir(path.join(__dirname, "logs"));
+		}
+		await fsPromises.appendFile(
+			path.join(__dirname, "logs", "eventLogs.txt"),
+			logItem
+		);
+	} catch (error) {
+		console.log("error");
+	}
+};
+
+const logger = (req, res, next) => {
+	logEvents(`${req.method} ${req.headers.origin} \t ${req.url}`);
+	console.log(`${req.method} ${req.path}`);
+	next();
+};
+module.exports = { logEvents, logger };
