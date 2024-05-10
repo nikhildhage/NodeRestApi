a906463518fbbc660e1cd6b380a9cc8da1a33426
nikhildhage <nikhil.dhage@gmail.com>
2024-05-09 19:30:51 -0700

    Updaaate server

--- server.js
+++ server.js
@@ -21,19 +21,18 @@ app.use(logger);
 
 app.use(cors(corsOptions));
 
+//Default ROute
 app.use("/", require("./routes/root"));
 
+//States API ROutes
+
+app.use("/state/", require("./routes/api/states"));
+
+app.use("/:state/funfact", require("./routes/api/states"));
+
+app.use("/:state/capital", require("./routes/api/states"));
+
 // Handle 404 Page NOt Found Errors for un-defined routes
-app.all("*", (req, res) => {
-	res.status(404);
-	if (req.accepts("html")) {
-		res.sendFile(path.join(__dirname, "views", "404.html"));
-	} else if (req.accepts("json")) {
-		res.json({ error: "404 Not Found" });
-	} else {
-		res.type("txt").send("404 Not Found");
-	}
-});
 
 // Error Logger
 app.use(errorHandler);
