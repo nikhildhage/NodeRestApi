330fc368b68705e820097fa9f019cd2219fe4e62
nikhildhage <nikhil.dhage@gmail.com>
2024-05-09 19:30:25 -0700

    Update states js

--- routes/api/states.js
+++ routes/api/states.js
@@ -3,7 +3,10 @@ const router = express();
 
 const stateController = require("../../controllers/statesController");
 
-router.route("/states/$").get(stateController.getAllStates);
+router.get("/states", stateController.getState);
+
 router.route("/:state").get(stateController.getState);
-router.route("/:state/funfact").get(stateController.getState);
+router.route("/:state/funfact").get(stateController.getStateFunFact);
+router.route("/:state/capital").get(stateController.getStateCapital);
+
 module.exports = router;
